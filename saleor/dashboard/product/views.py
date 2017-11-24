709fd35398668fc5fb2bb79213704c8dfa24ce42
michal <michal.sipa@mirumee.com>
2017-11-24 10:14:21 +0100

    Add filter for ProductClass
    Add ProductClassFilter to dashboard/product/filters.
    Update product_class_list view.
    Update dashboard/product/product_class/list.html template.

--- saleor/dashboard/product/views.py
+++ saleor/dashboard/product/views.py
@@ -5,18 +5,18 @@ from django.contrib.auth.decorators import permission_required
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
-from django.utils.translation import npgettext_lazy, pgettext_lazy, gettext
+from django.utils.translation import npgettext_lazy, pgettext_lazy
 from django.views.decorators.http import require_POST
 
 from ...core.utils import get_paginator_items
 from ...product.models import (
-    AttributeChoiceValue, Category, Product, ProductAttribute, ProductClass,
+    AttributeChoiceValue, Product, ProductAttribute, ProductClass,
     ProductImage, ProductVariant, Stock, StockLocation)
 from ...product.utils import (
     get_availability, get_product_costs_data, get_variant_costs_data)
 from ...settings import DASHBOARD_PAGINATE_BY
 from ..views import staff_member_required
-from .filters import ProductFilter
+from .filters import ProductFilter, ProductClassFilter
 from . import forms
 
 
@@ -25,16 +25,17 @@ from . import forms
 def product_class_list(request):
     classes = ProductClass.objects.all().prefetch_related(
         'product_attributes', 'variant_attributes').order_by('name')
+    class_filter = ProductClassFilter(request.GET, queryset=classes)
     form = forms.ProductClassForm(request.POST or None)
     if form.is_valid():
         return redirect('dashboard:product-class-add')
     classes = get_paginator_items(
-        classes, DASHBOARD_PAGINATE_BY, request.GET.get('page'))
+        class_filter.qs, DASHBOARD_PAGINATE_BY, request.GET.get('page'))
     classes.object_list = [
         (pc.pk, pc.name, pc.has_variants, pc.product_attributes.all(),
          pc.variant_attributes.all())
         for pc in classes.object_list]
-    ctx = {'form': form, 'product_classes': classes}
+    ctx = {'form': form, 'product_classes': classes, 'filter': class_filter}
     return TemplateResponse(
         request,
         'dashboard/product/product_class/list.html',
